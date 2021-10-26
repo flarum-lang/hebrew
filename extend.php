b3e93990257656aeaf9798fce8c0ff65ffeba0df
Dmitry Banny <dj.icecore@gmail.com>
2021-10-26 18:06:10 +0300

    Update extend.php

--- extend.php
+++ extend.php
@@ -1,7 +1,9 @@
 <?php
 
 /*
- * This file is part of the Hebrew language pack for Flarum.
+ * This file is part of Flarum Hebrew language.
+ * Author: Dmitry Banny
+ * Email: dbanny@duck.com
  *
  * For detailed copyright and license information, please view the
  * LICENSE file that was distributed with this source code.
